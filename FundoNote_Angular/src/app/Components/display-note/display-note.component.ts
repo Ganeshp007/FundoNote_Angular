92c6f49e388fa4294387557b488ca7cdf693cc3b
Ganeshp007 <potdart40.gp@gmail.com>
2022-08-12 13:15:16 +0530

    Added: Display All Note And Its API Integration with ParentChild DataSharing via input

--- FundoNote_Angular/src/app/Components/display-note/display-note.component.ts
+++ FundoNote_Angular/src/app/Components/display-note/display-note.component.ts
@@ -7,6 +7,8 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class DisplayNoteComponent implements OnInit {
  
+  @Input() NoteList:any;
+
   constructor() { }
 
   ngOnInit(): void {
